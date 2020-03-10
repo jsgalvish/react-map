e0c4e4f957fd658da409e47db08e70617cb17827
JS Galvis H <jsgalvish@unal.edu.co>
2020-03-10 10:46:21 -0500

    Updates

---
+++ precache-manifest.e25437ead75554844e2a6da8b95304ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba3f7a953a3c52749c7",
+    "url": "/react-map/static/css/main.8b38f553.chunk.css"
+  },
+  {
+    "revision": "5ba3f7a953a3c52749c7",
+    "url": "/react-map/static/js/main.7badc92d.chunk.js"
+  },
+  {
+    "revision": "ae5d9a88cabb3e5f777a",
+    "url": "/react-map/static/js/runtime~main.45ad3ed3.js"
+  },
+  {
+    "revision": "02a6f6a60d6dc61969ed",
+    "url": "/react-map/static/js/2.75e7e679.chunk.js"
+  },
+  {
+    "revision": "f99cca8cab0dff15e67560d3b8fdb84c",
+    "url": "/react-map/index.html"
+  }
+];
